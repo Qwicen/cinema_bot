2b2600479c14655405391d1eca497482fe3b70ff
Sergei Popov <popovsergey95@gmail.com>
2018-11-17 23:42:11 +0100

    echo bot

---
+++ application.py
@@ -0,0 +1,50 @@
+import logging
+import time
+import flask
+import telebot
+
+from config import API_TOKEN
+from config import WEBHOOK_SSL_CERT
+from config import WEBHOOK_URL_BASE
+from config import WEBHOOK_URL_PATH
+
+logger = telebot.logger
+telebot.logger.setLevel(logging.DEBUG)
+bot = telebot.TeleBot(API_TOKEN, threaded=False)
+app = flask.Flask(__name__)
+
+@app.route('/', methods=['GET', 'HEAD'])
+def index():
+    return ''
+
+
+@app.route(WEBHOOK_URL_PATH, methods=['POST'])
+def webhook():
+    if flask.request.headers.get('content-type') == 'application/json':
+        json_string = flask.request.get_data().decode('utf-8')
+        update = telebot.types.Update.de_json(json_string)
+        bot.process_new_updates([update])
+        return ''
+    else:
+        flask.abort(403)
+
+
+@app.route('/set_webhook', methods=['GET', 'POST'])
+def set_webhook():
+    bot.remove_webhook()
+    time.sleep(0.1)
+    bot.set_webhook(url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH,
+                    certificate=open(WEBHOOK_SSL_CERT, 'r'))
+    return ''
+
+
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    bot.reply_to(message, "Hello, " + message.from_user.first_name)
+
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo_message(message):
+    bot.send_message(message.chat.id, message.text)
+
+if __name__ == "__main__":
+    app.run()
