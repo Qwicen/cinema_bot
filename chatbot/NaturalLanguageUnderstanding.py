0a730682572a915ac531f09444b67bc9e6883979
Sergey Popov <popovsergey95@gmail.com>
2018-12-16 17:25:42 +0300

    ner implementation

--- chatbot/NaturalLanguageUnderstanding.py
+++ chatbot/NaturalLanguageUnderstanding.py
@@ -1,2 +1,20 @@
-def NamedEntityRecognition(message):
-    return message.split(), list()
+from deeppavlov import configs, build_model
+from deeppavlov import train_model
+
+class NER:
+
+    def __init__(self, train=False):
+        if train:
+            train_model("./deeppavlov/ner_config.json", download=True)
+        self.ner_model = build_model("./deeppavlov/ner_config.json", download=True)
+
+    def NamedEntityRecognition(self, message):
+        ner = self.ner_model([message])
+        return ner[0][0], ner[1][0]
+
+if __name__ == "__main__":
+    ner = NER(train=False)
+    msg = 'I want to watch an action film with Arnold Swarzenneger'
+    msg_split, ner_labels = ner.NamedEntityRecognition(msg)
+    print(msg_split)
+    print(ner_labels)
