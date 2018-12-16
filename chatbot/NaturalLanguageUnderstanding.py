8ed25bbd4fdfa64bc3e0a29e081e35112a075532
Sergey Popov <popovsergey95@gmail.com>
2018-12-16 21:28:22 +0300

    fix ner class

--- chatbot/NaturalLanguageUnderstanding.py
+++ chatbot/NaturalLanguageUnderstanding.py
@@ -3,18 +3,19 @@ from deeppavlov import train_model
 
 class NER:
 
-    def __init__(self, train=False):
-        if train:
-            train_model("./deeppavlov/ner_config.json", download=True)
-        self.ner_model = build_model("./deeppavlov/ner_config.json", download=True)
+    ner_model = build_model("./deeppavlov/ner_config.json", download=True)
 
-    def NamedEntityRecognition(self, message):
-        ner = self.ner_model([message])
+    def train():
+        train_model("./deeppavlov/ner_config.json", download=True)
+        NER.ner_model = build_model("./deeppavlov/ner_config.json", download=True)
+
+    def NamedEntityRecognition(message):
+        ner = NER.ner_model([message])
         return ner[0][0], ner[1][0]
 
 if __name__ == "__main__":
-    ner = NER(train=False)
     msg = 'I want to watch an action film with Arnold Swarzenneger'
-    msg_split, ner_labels = ner.NamedEntityRecognition(msg)
+    NER.train()
+    msg_split, ner_labels = NER.NamedEntityRecognition(msg)
     print(msg_split)
     print(ner_labels)
